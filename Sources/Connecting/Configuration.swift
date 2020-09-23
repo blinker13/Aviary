fbb869827bdef3a83b30721caf97ae54453d0102
Felix Gabel <felix.gabel@me.com>
2020-09-23 20:55:08 +0200

    Add Configuration type
    Signed-off-by: Felix Gabel <felix.gabel@me.com>

---
+++ Sources/Connecting/Configuration.swift
@@ -0,0 +1,21 @@
+
+public struct Configuration {
+	public var access: Access
+	public var location: Location
+}
+
+public extension Configuration {
+
+	static let inMemory = Configuration()
+	static let temporary = Configuration(location: .temporary)
+
+	init(location: Location = .inMemory, access: Access = .default) {
+		self.location = location
+		self.access = access
+	}
+
+	init(path: String, access: Access = .default) {
+		self.location = .path(path)
+		self.access = access
+	}
+}
