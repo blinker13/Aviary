9458f470fe92500ffa5e4e793c8dd98b15f96cd3
Felix Gabel <felix.gabel@me.com>
2020-09-23 14:38:59 +0200

    Add Access type
    Signed-off-by: Felix Gabel <felix.gabel@me.com>

---
+++ Sources/Connecting/Access.swift
@@ -0,0 +1,22 @@
+
+import SQLite3
+
+public struct Access: OptionSet {
+	public var rawValue: Int32
+
+	public init(rawValue: Int32) {
+		self.rawValue = rawValue
+	}
+}
+
+public extension Access {
+	static let create = Access(rawValue: SQLITE_OPEN_CREATE)
+
+	static let readwrite = Access(rawValue: SQLITE_OPEN_READWRITE)
+	static let readonly = Access(rawValue: SQLITE_OPEN_READONLY)
+
+	static let multiThread = Access(rawValue: SQLITE_OPEN_NOMUTEX)
+	static let serialized = Access(rawValue: SQLITE_OPEN_FULLMUTEX)
+
+	static let `default`: Access = [readwrite, create]
+}
